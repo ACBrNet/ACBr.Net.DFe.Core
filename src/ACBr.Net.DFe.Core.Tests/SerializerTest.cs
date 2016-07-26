afa88393866d25103fe2cb255f91e91f6d781dd0
RFTD <rfteno@gmail.com>
2016-07-25 23:06:10 -0400

    [+] Adicionado mais testes xUnit.

--- src/ACBr.Net.DFe.Core.Tests/SerializerTest.cs
+++ src/ACBr.Net.DFe.Core.Tests/SerializerTest.cs
@@ -1,6 +1,8 @@
 ﻿using ACBr.Net.DFe.Core.Serializer;
 using System;
 using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 using Xunit;
 
 namespace ACBr.Net.DFe.Core.Tests
@@ -49,6 +51,7 @@ namespace ACBr.Net.DFe.Core.Tests
 			}
 
 			xml.TestInterface1 = xml.XmlItems[0];
+			xml.TestInterface2 = xml.XmlItems[1];
 
 			return xml;
 		}
@@ -62,6 +65,20 @@ namespace ACBr.Net.DFe.Core.Tests
 			serializer.Serialize(xml, "teste.xml");
 
 			Assert.True(File.Exists("teste.xml"), "Erro ao serializar a classe");
+
+			var xmlDocument = XDocument.Load(File.Open("teste.xml", FileMode.Open));
+			Assert.NotNull(xmlDocument);
+
+			Assert.NotNull(xmlDocument.Root);
+			Assert.True(xmlDocument.Root?.Name == "RFTD", "Erro ao serializar root do Xml.");
+
+			Assert.True(xmlDocument.Root.HasAttributes, "Erro ao serializar atributos do root.");
+			Assert.True(xmlDocument.Root.Attributes().Count() == 1, "Erro ao serializar atributos do root.");
+			Assert.True(xmlDocument.Root.FirstAttribute.Name == "id", "Erro ao serializar atributo id do root.");
+			Assert.True(xmlDocument.Root.FirstAttribute.Value == "01", "Erro ao serializar atributo id do root.");
+
+			var nodes = xmlDocument.Root.Nodes();
+			Assert.True(nodes.Count() == 12, "Erro ao serializar dados do xml.");
 		}
 	}
 }
