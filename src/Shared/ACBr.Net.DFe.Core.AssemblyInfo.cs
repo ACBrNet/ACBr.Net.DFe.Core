ca2fcdbd118662bb22bc0b750760a9ebbed2c3db
RFTD <rfteno@gmail.com>
2018-06-22 13:58:03 -0400

    [+] Release 2.1.1.1 [-] Correção nas dependencias do pacote Nuget.

--- src/Shared/ACBr.Net.DFe.Core.AssemblyInfo.cs
+++ src/Shared/ACBr.Net.DFe.Core.AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
-[assembly: AssemblyInformationalVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.1")]
+[assembly: AssemblyInformationalVersion("1.2.1.1")]
